bdf9924f37c4e490500eff2551c60a7a028a947c
natanzeraa <contato@natanfelipe.com.br>
2026-05-20 18:08:24 -0300

    feat(api): adiciona client axios com interceptors
    - otp-auth-app/src/lib/axios.ts: cria instância do axios com baseURL e interceptors para anexar accessToken do AsyncStorage e redirecionar para /login em 401/403

---
+++ otp-auth-app/src/lib/axios.ts
@@ -0,0 +1,42 @@
+import AsyncStorage from '@react-native-async-storage/async-storage'
+import axios from 'axios'
+import { router } from 'expo-router'
+
+const AUTH_STORAGE_KEY = '@otp-auth-app:auth-state'
+
+export const api = axios.create({
+  baseURL: 'http://localhost:3001/api/v1'
+})
+
+api.interceptors.request.use(
+  async (config) => {
+    const publicRoutes = ['/auth/login', '/auth/signup', '/auth/refresh']
+
+    if (publicRoutes.includes(config.url ?? '')) {
+      return config
+    }
+
+    const storage = await AsyncStorage.getItem(AUTH_STORAGE_KEY)
+    console.log(storage)
+    const data = storage ? JSON.parse(storage) : null
+
+    if (data.accessToken) {
+      config.headers.Authorization = `Bearer ${data.accessToken}`
+    }
+
+    return config
+  },
+  (error) => Promise.reject(error)
+)
+
+api.interceptors.response.use(
+  (response) => response,
+  async (error) => {
+    if ([401, 403].includes(error.response?.status)) {
+      await AsyncStorage.removeItem('@otp-auth-app:auth-state')
+      router.replace('/login')
+    }
+
+    return Promise.reject(error)
+  }
+)
